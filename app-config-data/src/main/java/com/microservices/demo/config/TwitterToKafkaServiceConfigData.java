1addf0c38bbaca276edd6cc4ab1ef18e93a329ee
vishal Gorule <vishalgorule09@gmail.com>
2021-09-03 02:21:02 +0530

    First service

---
+++ app-config-data/src/main/java/com/microservices/demo/config/TwitterToKafkaServiceConfigData.java
@@ -0,0 +1,18 @@
+package com.microservices.demo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "twitter-to-kafka-service")
+public class TwitterToKafkaServiceConfigData {
+    private List<String> twitterKeywords;
+    private Boolean enableMockTweets;
+    private Integer mockMinTweetLength;
+    private Integer mockMaxTweetLength;
+    private Long mockSleepMs;
+}
