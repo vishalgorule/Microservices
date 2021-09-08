f19880c048cb4be24dab93580bf9fdfe50bf2e6f
vishal Gorule <vishalgorule09@gmail.com>
2021-09-08 16:54:05 +0530

    Config server docker

--- docker-compose/check-config-server-started.sh
+++ docker-compose/check-config-server-started.sh
@@ -15,4 +15,4 @@ while [[ ! $curlResult == "200" ]]; do
   curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
 done
 
-./cnb/lifecycle/launcher
+./cnb/process/web
