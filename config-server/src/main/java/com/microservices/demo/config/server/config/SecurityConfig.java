429feb7b4130b9b9051b0a705830c76d48b66818
vishal Gorule <vishalgorule09@gmail.com>
2021-09-09 01:39:23 +0530

    Config server docker

--- config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.microservices.demo.config.server.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
